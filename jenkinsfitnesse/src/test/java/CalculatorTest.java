4fbdd6db68c5c1d5914eed64152258f5dba2c424
Sebastian Lundgren <sebastian.lundgren@umu.se>
2019-04-17 13:43:36 +0200

    Lade till test.

---
+++ jenkinsfitnesse/src/test/java/CalculatorTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+import sebastian.Calculator;
+public class CalculatorTest {
+    @Test
+    public void testaAdd() {
+        Calculator calculator = new Calculator();
+        calculator.setNumber(8.0);
+        calculator.setNumber2(2.0);
+        assert(calculator.add() == 10.0);
+    }
+
+    @Test
+    public void testaSubtract() {
+        Calculator calculator = new Calculator();
+        calculator.setNumber(8.0);
+        calculator.setNumber2(2.0);
+        assert(calculator.subtract() == 6.0);
+    }
+
+    @Test
+    public void testaMultiply() {
+        Calculator calculator = new Calculator();
+        calculator.setNumber(8.0);
+        calculator.setNumber2(2.0);
+        assert(calculator.multiply() == 16.0);
+    }
+
+    @Test
+    public void testaDivide() {
+        Calculator calculator = new Calculator();
+        calculator.setNumber(8.0);
+        calculator.setNumber2(2.0);
+        assert(calculator.divide() == 4.0);
+    }
+}
